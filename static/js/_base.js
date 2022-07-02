54788c673a4e8e0f26ac025b45b6b78095f08cf8
SeonminKim1 <yubi5050@naver.com>
2022-07-03 04:53:58 +0900

    Add Base.js - for front-url, backend-url, const variable

---
+++ static/js/_base.js
@@ -0,0 +1,7 @@
+// For All
+const backend_base_url = "http://127.0.0.1:8000"
+const frontend_base_url = "http://127.0.0.1:5500"
+
+
+// For main.js, main_deatil.js
+const main_product_img_id= "main_product_"
