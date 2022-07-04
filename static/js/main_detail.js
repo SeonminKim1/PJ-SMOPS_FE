bcc54bb51d8cfd1d8d68aebedfac3deab4d374bd
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-07-04 23:52:51 +0900

    Style all page

--- static/js/main_detail.js
+++ static/js/main_detail.js
@@ -1,17 +1,17 @@
 
-async function loadMainProductDetailPage(){
+async function loadMainProductDetailPage() {
     console.log("main_detail.js - loadMainProductDetailPage")
 
     let product_id;
-    if(localStorage.getItem('product_img_id')){
+    if (localStorage.getItem('product_img_id')) {
         product_id = localStorage.getItem('product_img_id')
     }
     console.log('main_detail.js - product_id1', product_id)
     product_id = product_id.replace(MAIN_PROUCT_IMG_ID, '')
     product_id = parseInt(product_id)
     console.log('main_detail.js - product_id2', product_id)
-    const response = await fetch(`${backend_base_url}/product/detail/${product_id}`,{
-        headers:{
+    const response = await fetch(`${backend_base_url}/product/detail/${product_id}`, {
+        headers: {
             Accept: "application/json",
             'content-type': "application/json"
         },
@@ -19,9 +19,9 @@ async function loadMainProductDetailPage(){
         // body: JSON.stringify(Data)
     })
 
-    response_json = await response.json()  
+    response_json = await response.json()
 
-    if(response.status == 200) {
+    if (response.status == 200) {
         MainProductDetailPutData(response_json)
     } else {
         alert('ERROR: ', response.status)
@@ -31,9 +31,9 @@ async function loadMainProductDetailPage(){
 }
 
 
-function MainProductDetailPutData(product_one){
+function MainProductDetailPutData(product_one) {
     // Loading tag objects
-    const item_img = document.querySelector('.item-img-main-detail')
+    const item_img = document.querySelector('.img-main-detail')
     const title_detail = document.querySelector('.title-detail')
     const category_detail = document.querySelector('.category-detail')
     const img_shape_detail = document.querySelector('.img-shape-detail')
@@ -46,7 +46,7 @@ function MainProductDetailPutData(product_one){
 
     // json date data convert to 2022-06-29
     var created_date = new Date(product_one['created_date']);
-    var created_dateString = created_date.getFullYear() + '-' + (created_date.getMonth() + 1)  + '-' + created_date.getDate();
+    var created_dateString = created_date.getFullYear() + '-' + (created_date.getMonth() + 1) + '-' + created_date.getDate();
 
     // Put Value in html-tag-contents
     item_img.src = 'https:/' + product_one['img_path']
@@ -56,49 +56,49 @@ function MainProductDetailPutData(product_one){
     selling_user_detail.innerText = product_one['owner_user']
     img_shape_detail.innerText = product_one['img_shape']
     update_date_detail.innerText = created_dateString
-    price_detail.innerText = product_one['price'] +'원'
+    price_detail.innerText = product_one['price'] + '원'
     description_detail.text = product_one['description']
-    
+
     // json log data convert to logline
     var logs = product_one['log']
     var log_text = ''
 
-    for(var i=0; i<logs.length; i++){
+    for (var i = 0; i < logs.length; i++) {
         var update_date = new Date(logs[i]['updated_date']);
-        var update_dateString = update_date.getFullYear() + '-' + (update_date.getMonth() + 1)  + '-' + update_date.getDate();
-        if(i==0){
+        var update_dateString = update_date.getFullYear() + '-' + (update_date.getMonth() + 1) + '-' + update_date.getDate();
+        if (i == 0) {
             log_text = log_text + '아티스트:' + logs[i]['old_owner'] + '\n생성일자: ' + update_dateString + '\n\n'
-        }else{
-            log_text = log_text + '구매자:' + logs[i]['old_owner'] + '\n구매일자: ' + update_dateString + '\n구매금액:' + logs[i]['old_price'] + '원' +'\n\n'
+        } else {
+            log_text = log_text + '구매자:' + logs[i]['old_owner'] + '\n구매일자: ' + update_dateString + '\n구매금액:' + logs[i]['old_price'] + '원' + '\n\n'
         }
     }
 
     log_content_detail.innerText = log_text
 }
 
-async function buy_product(){
-    if(confirm("작품을 구매하시겠습니까?")){        
+async function buy_product() {
+    if (confirm("작품을 구매하시겠습니까?")) {
         // 입력받은 데이터 가져오기
         let product_id;
-        if(localStorage.getItem('product_img_id')){
+        if (localStorage.getItem('product_img_id')) {
             product_id = localStorage.getItem('product_img_id')
         }
         product_id = product_id.replace(MAIN_PROUCT_IMG_ID, '')
         product_id = parseInt(product_id)
 
-        const price = parseInt((document.querySelector('.price-detail').innerText).replace('원',''))
+        const price = parseInt((document.querySelector('.price-detail').innerText).replace('원', ''))
 
         const date = new Date();
-        const update_date = date.getFullYear() + '-' + (date.getMonth() + 1)  + '-' + date.getDate();
+        const update_date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
 
         const formData = {
-            product_id : product_id,
-            price : price,
-            updated_date : update_date
+            product_id: product_id,
+            price: price,
+            updated_date: update_date
         }
 
-        const response = await fetch(`${backend_base_url}/product/detail/buy/log/`,{
-            headers:{
+        const response = await fetch(`${backend_base_url}/product/detail/buy/log/`, {
+            headers: {
                 'content-type': "application/json",
                 "Authorization": "Bearer " + localStorage.getItem("access")
             },
@@ -108,7 +108,7 @@ async function buy_product(){
         console.log('==response', response)
         confirm('구매 완료 되었습니다.')
         window.location.replace(`${frontend_base_url}/templates/art/mygallery.html`);
-    }else{
+    } else {
         confirm('구매가 취소 되었습니다.')
     }
 }
--- static/js/user.js
+++ static/js/user.js
@@ -1,11 +1,11 @@
-async function join(){
+async function join() {
 
     // 입력받은 데이터 가져오기
     const joinData = {
-        email : document.getElementById("input-id-join").value,
-        fullname : document.getElementById("input-fullname-join").value,
-        password : document.getElementById("input-password-join").value,
-        password_confirm : document.getElementById("input-password-confirm").value,
+        email: document.getElementById("input-id-join").value,
+        fullname: document.getElementById("input-fullname-join").value,
+        password: document.getElementById("input-password-join").value,
+        password_confirm: document.getElementById("input-password-confirm").value,
     }
 
     // 입력받은 데이터를 BE서버에 회원가입 url로 request 요청
@@ -23,21 +23,22 @@ async function join(){
     respose_json = await response.json()
 
     // 정상적인 통신이 되었을 경우 = 회원가입 완료 > 로그인페이지로
-    if(response.status == 201) {
+    if (response.status == 201) {
+        alert("회원가입 완료!")
         window.location.replace(`${frontend_base_url}/templates/user/login.html`);
     } else {
         alert(response.status)
     }
-    
+
 
 }
 
 async function login() {
     const loginData = {
-        email : document.getElementById("input-id-init").value,
-        password : document.getElementById("input-password-init").value,
+        email: document.getElementById("input-id-init").value,
+        password: document.getElementById("input-password-init").value,
     }
-    
+
     const response = await fetch(`${backend_base_url}/user/api/token/`, {
         headers: {
             Accept: "application/json",
@@ -49,7 +50,7 @@ async function login() {
 
     response_json = await response.json()
 
-    if (response.status == 200){
+    if (response.status == 200) {
         // 로컬스토리지에 jwt access 토큰과 refresh 토큰 저장
         localStorage.setItem("access", response_json.access)
         localStorage.setItem("refresh", response_json.refresh)
@@ -57,12 +58,13 @@ async function login() {
         // 파싱하는 부분 복사해서 사용하기! 
         const base64Url = response_json.access.split('.')[1];
         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
 
         localStorage.setItem("payload", jsonPayload);
         // window.location.replace(`${frontend_base_url}/`);
+        alert("환영합니다!")
         window.location.replace(`${frontend_base_url}/templates/art/main.html`);
     } else {
         alert(response.status)
