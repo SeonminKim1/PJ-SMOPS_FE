0fe55d0d47b1e193f41540f8bd9ed9b45a94aa57
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-07-06 13:56:10 +0900

    .

--- static/js/_base.js
+++ static/js/_base.js
@@ -1,6 +1,6 @@
 // For All
-const backend_base_url = "http://127.0.0.1:8000"
-// const backend_base_url = "http://3.34.3.15:8000"
+// const backend_base_url = "http://127.0.0.1:8000"
+const backend_base_url = "http://3.34.3.15:8000"
 const frontend_base_url = "http://127.0.0.1:5500"
 
 
